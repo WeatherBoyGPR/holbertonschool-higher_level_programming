ea3f034e5501698c535ae6dd8442939e42b69b70
Gino Rossi <gp.rossijr@gmail.com>
2021-09-09 01:35:47 +0000

    updated 1-last_digit.py to specify numbers through numerals rather than words

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ print("Last digit of {} is ".format(number), end='')
 if (number % 10) > 0:
     number = -number
 if (number % 10) > 5:
-    print("{} and is greater than five".format(number % 10))
+    print("{} and is greater than 5".format(number % 10))
 elif (number % 10) == 0:
-    print("{} and is zero".format(number % 10))
+    print("{} and is 0".format(number % 10))
 else:
     print("{} and is less than 6 and not 0".format(number % 10))
