dd0a505d3b833da8dcfe5722d93601724bee2e4d
Gino Rossi <gp.rossijr@gmail.com>
2021-09-09 19:07:12 +0000

    added 10-check_cycle.c with lists.h header file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - will check for a cycle in a singly linked list
+ * @list: a node in the list to check
+ *
+ * Return: 1 if there is a cycle, 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *head = NULL, *posi = NULL;
+
+	head = list, posi = list;
+	if (list != NULL)
+	{
+		do {
+			posi = posi->next;
+		} while (posi != NULL && posi != head);
+	}
+	if (posi == head)
+		return (1);
+	return (0);
+}
