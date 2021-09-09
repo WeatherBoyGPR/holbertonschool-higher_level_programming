a00e3215057886f495f282923cb874afcc938f31
Gino Rossi <gp.rossijr@gmail.com>
2021-09-09 03:38:37 +0000

    updated 1-last_digit.py to properly account for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 print("Last digit of {} is ".format(number), end='')
-if (number % 10) > 0:
-    number = -number
-if (number % 10) > 5:
-    print("{} and is greater than 5".format(number % 10))
-elif (number % 10) == 0:
-    print("{} and is 0".format(number % 10))
+if (number) < 0:
+    numberend = -(-number % 10)
 else:
-    print("{} and is less than 6 and not 0".format(number % 10))
+    numberend = number % 10
+if (numberend) > 5:
+    print("{} and is greater than 5".format(numberend))
+elif (number % 10) == 0:
+    print("{} and is 0".format(numberend))
+else:
+    print("{} and is less than 6 and not 0".format(numberend))
