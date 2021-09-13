e0dc7145082e01640f126d083a3e5ca1b0fb3f28
Gino Rossi <gp.rossijr@gmail.com>
2021-09-13 18:18:22 +0000

    updated 2-args.py to only import argc variable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import sys
+    from sys import argv
 
-    idx = len(sys.argv) - 1
+    idx = len(argv) - 1
     if idx > 0:
         if idx is 1:
             print("1 argument:")
         else:
             print("{} arguments:".format(idx))
         for num in range(idx):
-            print("{}: {}".format(num + 1, sys.argv[num + 1]))
+            print("{}: {}".format(num + 1, argv[num + 1]))
             idx -= 1
     else:
         print("0 arguments.")
