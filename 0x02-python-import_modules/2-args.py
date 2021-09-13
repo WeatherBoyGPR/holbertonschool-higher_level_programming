f7c406582349cba137b426be57891a77e2d74f9f
Gino Rossi <gp.rossijr@gmail.com>
2021-09-13 18:15:33 +0000

    added 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-import sys
-idx = len(sys.argv)
-print("{}".format(idx))
-while idx != 0:
-    print("{}".format(idx))
-    idx -= 1
+if __name__ == '__main__':
+    import sys
+
+    idx = len(sys.argv) - 1
+    if idx > 0:
+        if idx is 1:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(idx))
+        for num in range(idx):
+            print("{}: {}".format(num + 1, sys.argv[num + 1]))
+            idx -= 1
+    else:
+        print("0 arguments.")
