3ce18bfeaf3fd1c088512f0b3422dd43470161bb
Gino Rossi <gp.rossijr@gmail.com>
2021-09-15 19:06:20 +0000

    updated 6-print_matrix_integer.py to print newline on empty string

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,3 +7,5 @@ def print_matrix_integer(matrix=[[]]):
                     print("{:d} ".format(matrix[y][x]), end='')
                 else:
                     print("{:d}".format(matrix[y][x]))
+    else:
+        print("")
