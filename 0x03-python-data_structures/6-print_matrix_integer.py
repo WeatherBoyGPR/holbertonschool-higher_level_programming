ba20069a940d7f2c3ff1bae1a413588f4835815f
Gino Rossi <gp.rossijr@gmail.com>
2021-09-15 19:14:04 +0000

    modified newline printing for 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,6 +6,5 @@ def print_matrix_integer(matrix=[[]]):
                 if x < len(matrix[y]) - 1:
                     print("{:d} ".format(matrix[y][x]), end='')
                 else:
-                    print("{:d}".format(matrix[y][x]))
-    else:
-        print("")
+                    print("{:d}".format(matrix[y][x]), end='')
+            print("")
