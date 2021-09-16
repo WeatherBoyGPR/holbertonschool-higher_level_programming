2c2046c3c2d4beaa8d6620a6f67f661f75b7a541
Gino Rossi <gp.rossijr@gmail.com>
2021-09-16 02:19:41 +0000

    updated 9-max_integer.py to remove isinstance check

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) <= 0 or isinstance(my_list, list) == 0:
+    if len(my_list) <= 0:
         return "None"
-    res = 0
+    res = my_list[0]
     for i in my_list:
         if i > res:
             res = i
