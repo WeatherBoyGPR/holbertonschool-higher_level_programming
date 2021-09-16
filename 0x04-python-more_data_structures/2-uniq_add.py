44e582451acf75fdb14f7be97982feb9429f6a5e
Gino Rossi <gp.rossijr@gmail.com>
2021-09-16 22:34:23 +0000

    added 2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    res = my_list[0]
+    for x in range(len(my_list)):
+        for y in range(x - 1, -1, -1):
+            if my_list[x] == my_list[y]:
+                break
+            if y == 0:
+                res += my_list[x]
+    return res
