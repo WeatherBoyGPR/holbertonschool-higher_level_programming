b40f795a70a60919c7329e2489d16c01d20b5cbf
Gino Rossi <gp.rossijr@gmail.com>
2021-09-20 12:57:05 +0000

    added 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,9 @@ for i in range(1, 101):
     if i % 15 == 0:
         print("FizzBuzz ", end='')
     elif i % 5 == 0:
-        print("Buzz ", end = '')
+        print("Buzz ", end='')
     elif i % 3 == 0:
-        print("Fizz ", end = '')
+        print("Fizz ", end='')
     else:
         print("{} ".format(i), end='')
     if i >= 100:
