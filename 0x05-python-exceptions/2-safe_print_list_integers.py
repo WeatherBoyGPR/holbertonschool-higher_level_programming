58acb090b35cfbeec413cd32b3bd34c03c4da9a2
Gino Rossi <gp.rossijr@gmail.com>
2021-09-27 18:58:26 +0000

    updated 2-safe_print_list_integers.py to have accurate variable names

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list_integers(mylist=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     res = 0
     for i in range(x):
         try:
-            print("{:d}".format(mylist[i]), end='')
+            print("{:d}".format(my_list[i]), end='')
             res += 1
         except (ValueError, TypeError, IndexError):
             pass
