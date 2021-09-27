32436a61ea5f24c9b9039e185c6c8b0d2ea57ade
Gino Rossi <gp.rossijr@gmail.com>
2021-09-27 17:53:13 +0000

    added 2-safe_print_list_integrs.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(mylist=[], x=0):
+    res = 0
+    num = 0
+    for i in mylist:
+        if num >= x:
+            break
+        try:
+            print("{:d}".format(i))
+        except:
+            pass
+        else:
+            res += 1
+        finally:
+            num += 1
+    return res
