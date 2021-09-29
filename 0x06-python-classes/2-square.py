233045332c9d08e588d0518394e2ac9a99686b5e
Gino Rossi <gp.rossijr@gmail.com>
2021-09-29 17:29:50 +0000

    added 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""1-square.py"""
+
+
+class Square:
+    """ Acts as a sqaure."""
+    def __init__(self, size=0):
+        """ Initiliazes class
+
+        Args:
+            size (int): Size of created square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
