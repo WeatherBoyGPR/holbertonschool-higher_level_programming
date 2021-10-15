d83b257e5dc6e4aaeef969b689a07934d0ec21a5
Gino Rossi <gp.rossijr@gmail.com>
2021-10-15 21:01:03 +0000

    Added 1-my_list.py with test file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,5 +3,8 @@
 
 
 class MyList(list):
-    """ Is a class... ic. """
-    pass
+    """ Adds sorted printing to list class """
+
+    def print_sorted(self):
+        """ Will print a sorted list """
+        print(sorted(self))
