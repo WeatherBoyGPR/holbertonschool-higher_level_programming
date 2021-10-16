22b9dc4a23ed5600384509083dcfcc8342ad26b2
Gino Rossi <gp.rossijr@gmail.com>
2021-10-16 22:42:08 +0000

    Added 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" contains BaseGeometry class v2 """
+
+
+class BaseGeometry():
+    """ still does nothing """
+
+    def area(self):
+        """ does nothing but complain """
+        raise Exception("area() is not implemented")
