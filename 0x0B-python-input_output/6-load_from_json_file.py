8e1f2c6178cf278fb1ec87f49a5e13b67fd37dad
Gino Rossi <gp.rossijr@gmail.com>
2021-10-18 16:35:20 +0000

    added 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Contains load_from_json_file function """
+import json
+
+
+def load_from_json_file(filename):
+    """ Will load a single object from a json file """
+    with open(filename, encoding='utf-8') as f:
+        return json.loads(f.readline())
