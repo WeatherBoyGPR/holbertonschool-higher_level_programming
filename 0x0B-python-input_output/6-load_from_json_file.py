4cd83cd4def57293d359dec8253c3a3a76112dca
Gino Rossi <gp.rossijr@gmail.com>
2021-10-18 18:45:51 +0000

    updated 6-load_from_json_file.py to scan entire file rather than single line

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """ Will load a single object from a json file """
     with open(filename, encoding='utf-8') as f:
-        return json.loads(f.readline())
+        return json.loads(f)
