3fbd534a2f4f0d8ccc5102c7c0dc5104bedb8e99
Gino Rossi <gp.rossijr@gmail.com>
2021-10-19 15:34:37 +0000

    added __init__.py file, also added Base Class with __init__ function

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
 """ Contains Base class """
+
+class Base():
+    """ Base
+    Will serve as a base for rectangle and square classes
+    Cls Attributes:
+        nb_objects (int) - total number of objects
+    Obj Attributes:
+        id (int): simple id of the object
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ __init__
+        Args:
+            id (int)/(None): object id, defaults to nb_objects+1 on None
+        """
+        self.id = id
+        Base.__nb_objects += 1
+        if id is None:
+            self.id = Base.__nb_objects
