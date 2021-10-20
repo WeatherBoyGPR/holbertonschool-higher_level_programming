c4178b7d8e08a5271b27b6e47317fc93af602a51
Gino Rossi <gp.rossijr@gmail.com>
2021-10-20 13:44:54 +0000

    updated square.py to have getter and setter methods for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,6 +12,7 @@ class Square(Rectangle):
         nb_objects (int) - total number of objects
     Obj Attributes:
         id: simple id of the object
+        size (int) > 0: size of Square
         width (int) > 0: width of Square
         height (int) > 0: height of Square
         x (int) >= 0: x coord of Square
@@ -21,6 +22,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ __init__
         Args:
+            size (int) > 0: size of Square
             width (int) > 0: width of Square
             height (int) > 0: height of Square
             x (int): x position of Square
@@ -35,3 +37,15 @@ class Square(Rectangle):
             "[Square] (" + str(self.id) + ") " + str(self.x) + "/" +
             str(self.y) + " - " + str(self.height)
         )
+
+    @property
+    def size(self):
+        """ Getter for size """
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        """ Setter for size """
+        self.integer_validation(value, "width")
+        self.height = value
+        self.width = value
