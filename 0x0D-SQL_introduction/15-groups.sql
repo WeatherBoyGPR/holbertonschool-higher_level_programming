f3a19be8efe526efc9bd1be5ecb701782050f86a
Gino Rossi <gp.rossijr@gmail.com>
2021-11-18 23:31:36 +0000

    updated 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Will list total number of records with the same score in second_table
-SELECT score, COUNT(SCORE) FROM second_table GROUP BY score HAVING COUNT(score) > 1;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score HAVING COUNT(score) > 1 ORDER BY number DESC;
