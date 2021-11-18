ba9f735718d7568ffb51579b2ca0b2bbaa449990
Gino Rossi <gp.rossijr@gmail.com>
2021-11-18 19:28:14 +0000

    updated 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Will create table second_table, adds multiple rows
-CREATE TABLE second_table (id INT,
+CREATE TABLE IF NOT EXISTS `second_table` (id INT,
 name VARCHAR(256),
 score INT);
 INSERT INTO `second_table`(`id`, `name`, `score`) VALUES (1, "John", 10);
