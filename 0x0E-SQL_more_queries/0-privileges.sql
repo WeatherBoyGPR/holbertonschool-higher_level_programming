02fbd27cfdd38a237712b4951f116a36594144af
Gino Rossi <gp.rossijr@gmail.com>
2021-11-21 03:25:39 +0000

    modified 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 --Will list all privileges of user_0d_1 and user_0d_2 in localhost
-SHOW GRANTS FOR user_0d_1@localhost, user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
