0f886e8e5d8f90f50065fced0843437ca88f077e
Gino Rossi <gp.rossijr@gmail.com>
2021-11-22 00:27:51 +0000

    added 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,7 @@
 -- Will create user user_0d_1 , grants all privileges
-
-CREATE USER
+CREATE USER 
 	IF NOT EXISTS 'user_0d_1'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-	ON *.*
-	TO 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
+	ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
