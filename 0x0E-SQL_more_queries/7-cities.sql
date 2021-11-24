d42112940b52f9a3202a07801cbf6b63655825cb
WeatherBoyGPR <gp.rossijr@gmail.com>
2021-11-24 23:22:12 +0000

    updated 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,7 +4,7 @@
 -- state_id INT, NOT NULL, FOREIGN KEY(id in states table)
 -- name VARCHAR(256), NOT NULL
 CREATE DATABASE
-	IF NOT EXISTS `hbtn_0d_usa`
+	IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE
 	IF NOT EXISTS `hbtn_0d_usa`.`cities`(
 	PRIMARY_KEY(`id`),
