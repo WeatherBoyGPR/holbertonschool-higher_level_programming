353a47c4af2469dd6719b2ba33601e0e57e3193a
WeatherBoyGPR <gp.rossijr@gmail.com>
2021-11-25 03:20:49 +0000

    updated 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- Will list all cities of california in database hbtn_0d_usa
 SELECT id, name
-	FROM `hbtn_0d_usa`.`cities`
+	FROM `cities`
 	WHERE state_id =
 		(SELECT id
-		FROM `hbtn_0d_usa`.`states`
-		WHERE name = 'California')
+		FROM `states`
+		WHERE name = "California")
 	ORDER BY `id`;
