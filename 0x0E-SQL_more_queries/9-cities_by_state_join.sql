db30e34093003969f1dee087a9f7c70b5f01f600
WeatherBoyGPR <gp.rossijr@gmail.com>
2021-11-25 04:12:55 +0000

    added 9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Will list all cites contain in database hbtn_0d_usa
+SELECT `cities`.`id`, `cities`.`name`, `states`.`name`,
+	FROM cities
+	JOIN states ON `cities`.`state_id` = `states`.`id`
+	ORDER BY `cities`.`id` ASC;
