518e0434a9b209ab548a537482b6b2fa0ded988b
WeatherBoyGPR <gp.rossijr@gmail.com>
2021-11-25 03:13:11 +0000

    added 8-cities_of_california_subquery.sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- Will list all cities of california in database hbtn_0d_usa
+SELECT id, name
+	FROM `hbtn_0d_usa`.`cities`
+	WHERE state_id = (
+		SELECT id
+		FROM `hbtn_0d_usa`.`states`
+		WHERE name = 'California'
+	);
