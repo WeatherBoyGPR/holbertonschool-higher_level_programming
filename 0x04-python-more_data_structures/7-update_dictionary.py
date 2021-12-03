911510a7c0a79eb6efd902fddf0434159ece5cf1
WeatherBoyGPR <gp.rossijr@gmail.com>
2021-12-03 23:23:10 +0000

    added 7-updated_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# holds update_dictionary function
+
+def update_dictionary(a_dictionary, key, value):
+    """Updates or adds a dictionary with a key/value pair"""
+    a_dictionary[key] = value
+    return (a_dictionary)
