aa974b194495b2790a9894f3f36f1263cccebf71
WeatherBoyGPR <gp.rossijr@gmail.com>
2021-12-15 20:14:14 +0000

    added 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
 let stri = 'No argument';
-if (process.argv.length >= 2) {
+if (process.argv.length > 2) {
   stri = 'Arguments found';
+} else if (process.argv.length === 1) {
+  stri = 'Argument found';
 }
 console.log(stri);
