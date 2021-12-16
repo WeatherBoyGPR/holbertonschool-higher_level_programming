f4e6f271e3661ea41b5e4a4a75209af00dcdfac5
WeatherBoyGPR <gp.rossijr@gmail.com>
2021-12-16 02:13:09 +0000

    updated 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const err = 'Missing number of occurrences';
 const nu = Math.floor(Number(process.argv[2]));
 
 if (nu) {
-  for (let i = 0; i <= nu; i++) {
+  for (let i = 0; i < nu; i++) {
     console.log('C is fun');
   }
 } else {
