793a78f535cfddb8a1909ee8ad2c1564de7caa20
WeatherBoyGPR <gp.rossijr@gmail.com>
2021-12-17 22:08:57 +0000

    added 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,18 +5,17 @@ let res = 0;
 const len = process.argv.length;
 
 if (len > 3) {
-  if (process.argv[2] >= process.argv[3]) {
-    gre = process.argv[2];
-    res = process.argv[3];
-  } else {
-    gre = process.argv[3];
-    res = process.argv[2];
-  }
+  gre = process.argv[2];
+  res = gre;
 
-  for (let i = 4; i < len; i++) {
-    if (process.argv[i] > gre) {
-      res = gre;
-      gre = process.argv[i];
+  for (let i = 3; i < len; i++) {
+    if (Number(process.argv[i]) > gre) {
+      gre = Number(process.argv[i]);
+    }
+  }
+  for (let i = 3; i < len; i++) {
+    if (Number(process.argv[i]) < gre && Number(process.argv[i]) > res) {
+      res = Number(process.argv[i]);
     }
   }
 }
