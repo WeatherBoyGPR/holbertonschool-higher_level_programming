25e867e6928cbf70664974a47bb254702dd4233f
WeatherBoyGPR <gp.rossijr@gmail.com>
2021-12-18 04:43:34 +0000

    updated 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,8 +5,13 @@ let res = 0;
 const len = process.argv.length;
 
 if (len > 3) {
-  gre = Number(process.argv[2]);
-  res = Number(process.argv[3]);
+  if (Number(process.argv[2]) >= Number(process.argv[3])) {
+    gre = Number(process.argv[2]);
+    res = Number(process.argv[3]);
+  } else {
+    gre = Number(process.argv[3]);
+    res = Number(process.argv[2]);
+  }
 
   for (let i = 3; i < len; i++) {
     if (Number(process.argv[i]) > gre) {
