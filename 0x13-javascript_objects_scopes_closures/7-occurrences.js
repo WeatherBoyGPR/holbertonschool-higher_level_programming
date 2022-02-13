130a580a17659642790d713810eb6fdcbda9d26c
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-02-13 23:03:52 +0000

    added 7-occurrences.js

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let res = 0;
+  for (const i of list) {
+    if (i === searchElement)
+      res++;
+  }
+  return (res);
+}
