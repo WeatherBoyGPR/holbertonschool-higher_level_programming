a505363466659c6dc56d95485a63506cdd425e1c
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-02-13 18:11:42 +0000

    update 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,11 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (typeof w === "number" && typeof h === "number")
-      if (w > 0 && h > 0)
-      {
+    if (typeof w === 'number' && typeof h === 'number') {
+      if (w > 0 && h > 0) {
         this.width = w;
         this.height = h;
       }
+    }
   }
 };
