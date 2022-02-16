5a4b78773e419e98c9f4819118d44a7dddf758ed
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-02-16 16:19:45 +0000

    added 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+if (process.argv.length >= 3) {
+  fs.readFile(process.argv[2], 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
+}
