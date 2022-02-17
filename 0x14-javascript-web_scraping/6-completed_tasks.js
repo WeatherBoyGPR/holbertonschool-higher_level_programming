6cc3d0f6675b546dc25a8b1083b1fd02b728f594
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-02-17 22:55:09 +0000

    Added 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/* Will compute and print number of completed tasks, sorted by id
+ * arg1 - API URL
+ */
+
+const Request = require('request');
+
+if (process.argv.length < 3) {
+  console.log('invalid number of args');
+} else {
+  Request(process.argv[2] + '?completed=true', (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    const list = JSON.parse(body);
+    const res = {};
+
+    for (const i in list) {
+      if (res[String(list[i].userId)] === undefined) {
+        res[String(list[i].userId)] = 1;
+      } else {
+        res[String(list[i].userId)] += 1;
+      }
+    }
+    console.log(res);
+  });
+}
