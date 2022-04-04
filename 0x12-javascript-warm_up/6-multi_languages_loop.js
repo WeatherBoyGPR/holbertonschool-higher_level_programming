7c8cbf9f06f97375c0f5de1dc2e303ca95a4baa2
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-04 14:57:47 +0000

    added 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// will print 3 lines while making use of an array and a loop
+
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
