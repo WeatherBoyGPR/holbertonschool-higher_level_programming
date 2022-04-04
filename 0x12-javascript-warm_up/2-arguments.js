fabc7075d45373be502b8c4332a6d1d4efd0c71f
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-04 14:22:16 +0000

    added 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -6,13 +6,13 @@ const process = require('process');
 const num = process.argv.length - 2;
 
 switch (num) {
-	case 0:
-		console.log('No argument');
-		break;
-	case 1:
-		console.log('Argument found');
-		break;
-	default:
-		console.log('Arguments found');
-		break;
+  case 0:
+    console.log('No argument');
+    break;
+  case 1:
+    console.log('Argument found');
+    break;
+  default:
+    console.log('Arguments found');
+    break;
 }
