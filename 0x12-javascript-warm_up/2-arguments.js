f66021fe481481fd8770f4a7deb9daa03d64a94d
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-04 14:21:24 +0000

    added 2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Will print a message depending on number of arguments passed to script
+
+const process = require('process');
+
+const num = process.argv.length - 2;
+
+switch (num) {
+	case 0:
+		console.log('No argument');
+		break;
+	case 1:
+		console.log('Argument found');
+		break;
+	default:
+		console.log('Arguments found');
+		break;
+}
