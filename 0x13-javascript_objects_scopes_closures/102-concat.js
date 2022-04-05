8deb9725e9e0dcc6f1324fb04fa42e01c6ab43d0
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-05 17:53:23 +0000

    added 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,12 +4,8 @@
 const process = require('process');
 const fs = require('fs');
 
-console.log(process.argv[2], process.argv[3], process.argv[4]);
-
 const fileA = fs.readFileSync(process.argv[2]).toString();
 const fileB = fs.readFileSync(process.argv[3]).toString();
-
-console.log(fileA + fileB);
 
 fs.writeFile(process.argv[4], fileA + fileB, function (err) {
   if (err) { return console.error(err); }
