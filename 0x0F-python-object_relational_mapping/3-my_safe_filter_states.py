82b40788b907c1bb994d4d5f06845d6c013f3de6
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-07 16:29:32 +0000

    Added 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# Will list all states from the database hbtn_0e_0_usa
+
+import MySQLdb
+import sys
+import re
+
+if __name__ == "__main__":
+
+    argIn = [sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]]
+    query = '''SELECT * FROM `states` \
+            HAVING `name`= '{}'\
+            ORDER BY `id` ASC'''
+    state = re.search('^[\\w\\d]+', argIn[3]).group()
+
+    print(state)
+
+    DB = MySQLdb.connect('localhost', argIn[0], argIn[1], argIn[2], port=3306)
+    CR = DB.cursor()
+
+    CR.execute(query.format(state))
+    data = CR.fetchall()
+
+    for i in data:
+        print(i)
+
+    CR.close()
+    DB.close()
