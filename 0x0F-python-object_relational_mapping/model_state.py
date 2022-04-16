500dfbc29508ebcdce4374e13a1940cab7ad5377
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-16 03:12:19 +0000

    added model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Contains State class """
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ Class for state sdl storage """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
