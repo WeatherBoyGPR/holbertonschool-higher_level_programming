d89b541bcae2a2ea11bb6a6ab014ebf904f02612
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-16 03:57:10 +0000

    added 8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Will print first State object from database hbtn_0e_6_usa """
+
+from model_state import Base, State
+from sys import argv
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    sqlstr = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    sqlstr = sqlstr.format(argv[1], argv[2], argv[3])
+    engine = create_engine(sqlstr, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+
+    session = Session()
+    state = session.query(State)[0]
+    print("{}: {}".format(state.id, state.name))
