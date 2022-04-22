c370b0fe6be858cfa5ef383083f091a74c490ad5
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-22 17:48:18 +0000

    added 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Will send post request containing email to specified url """
+
+from sys import argv
+from urllib.request import urlopen, Request
+from urllib import parse
+
+if __name__ == "__main__":
+    data = {'email': argv[2]}
+    url = argv[1]
+
+    data = parse.urlencode(data)
+    data = data.encode('ascii')
+    req = Request(url, data)
+    with urlopen(req) as res:
+        print(res.read().decode('utf'))
