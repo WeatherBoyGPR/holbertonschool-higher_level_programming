9d6b12e67cb0b8883836b7ac6f82063c847c0003
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-22 17:04:05 +0000

    modified printing in file 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,13 +7,9 @@ from urllib.request import urlopen
 if __name__ == "__main__":
 
     url = 'https://intranet.hbtn.io/status'
-    form = """Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}"""
     with urlopen(url) as res:
         data = res.read()
-        typ = type(data)
-        con = data
-        utf = data.decode('utf')
-        print(form.format(typ, con, utf))
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- uf8 content:{}".format(data.decode('utf')))
