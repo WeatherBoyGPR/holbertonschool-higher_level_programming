bc8cddf90fe1255d2bd95d52c2ace27fc0979c27
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-23 22:02:25 +0000

    added 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Will list commits from repository owned by specified user """
+
+from sys import argv
+from requests.auth import HTTPBasicAuth
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos"
+    repo = '/' + argv[2]
+    owner = '/' + argv[1]
+    url = url + owner + repo + '/commits'
+
+    headers = {'page': '1'}
+
+    r = requests.get(url, headers=headers)
+    try:
+        res = r.json()
+        for i in range(0, 10):
+            sha = res[i].get('sha')
+            author = res[i].get('author').get('login')
+            print("{}: {}".format(sha, author))
+    except IndexError:
+        pass
