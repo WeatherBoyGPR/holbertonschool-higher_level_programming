80e1c9f488775f384d16b17b52c0542ade8242ff
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-23 17:07:46 +0000

    updated print statement in task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,5 +10,4 @@ if __name__ == "__main__":
     r = requests.get(url)
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
-    else
-        print(r.text)
+    print(r.text)
