3382072718271b96252d0073e7af4d657bbeab6e
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-23 22:22:34 +0000

    updated 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,18 +2,16 @@
 """ Will list commits from repository owned by specified user """
 
 from sys import argv
-from requests.auth import HTTPBasicAuth
 import requests
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos"
     repo = '/' + argv[2]
     owner = '/' + argv[1]
+
     url = url + owner + repo + '/commits'
 
-    headers = {'page': '1'}
-
-    r = requests.get(url, headers=headers)
+    r = requests.get(url)
     try:
         res = r.json()
         for i in range(0, 10):
