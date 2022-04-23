4f986c173812467b79c04e1790d01c9166a338fd
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-23 17:33:34 +0000

    added 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+""" Will take in letter as input and send post request """
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    val = ""
+    if len(argv) >= 2:
+        val = argv[1]
+    r = requests.post(url, data={'q': val})
+
+    try: res = r.json()
+    except:
+        print("Not a valid JSON")
+    else:
+        if len(res) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(res['id'], res['name']))
